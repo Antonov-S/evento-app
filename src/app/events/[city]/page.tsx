6ebefce7f9523c2592d31ce0de7c7413e4deffb7
Antonov-S <anto1nov1@gmail.com>
2024-02-28 23:29:40 +0200

    Change the H1 depending on the city param in the URL

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,20 @@
 import H1 from "@/components/h1";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
   return (
-    <main>
-      <H1>Find events in Siattle</H1>
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === "all" && "All events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
     </main>
   );
 }

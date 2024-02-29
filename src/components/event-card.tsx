976d803ad2fc8a83bfb01d840d2114081690e179
Antonov-S <anto1nov1@gmail.com>
2024-02-29 23:12:55 +0200

    Implement the EventCard component

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { EventoEvent } from "@/app/lib/types";
 
 type EventCardProp = {
@@ -5,5 +7,20 @@ type EventCardProp = {
 };
 
 export default function EventCard({ event }: EventCardProp) {
-  return <section>{event.name}</section>;
+  return (
+    <section className="flex flex-col flex-1 basis-80 h-[380px] w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
+      <Image
+        src={event.imageUrl}
+        alt={event.name}
+        width={500}
+        height={280}
+        className="h-[60%] object-fit"
+      />
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h2 className="text-2xl font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">By {event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">{event.location}</p>
+      </div>
+    </section>
+  );
 }

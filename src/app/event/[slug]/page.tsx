b8aecabe327af3391043ebf26b83fe687e616088
Antonov-S <anto1nov1@gmail.com>
2024-03-01 18:06:30 +0200

    Get the event slug from the URL and fetch the correct event

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,13 @@
-export default function EventPage() {
+import { FETCH_URL } from "@/lib/constants";
+
+type EventPageProp = {
+  params: { slug: string };
+};
+
+export default async function EventPage({ params }: EventPageProp) {
+  const slug = params.slug;
+  const response = await fetch(`${FETCH_URL}/${slug}`);
+  const event = await response.json();
+
   return <main>EventPage</main>;
 }

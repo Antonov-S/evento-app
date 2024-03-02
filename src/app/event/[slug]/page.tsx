33fd1aec466075c05d7867e7885b8d9cf059e0b9
Antonov-S <anto1nov1@gmail.com>
2024-03-03 00:12:41 +0200

    Finish the intro section of the event page and extract the hover effect to a reusable class with the @apply rule

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+
 import { FETCH_URL } from "@/lib/constants";
+import H1 from "@/components/h1";
 
 type EventPageProp = {
   params: { slug: string };
@@ -9,5 +12,53 @@ export default async function EventPage({ params }: EventPageProp) {
   const response = await fetch(`${FETCH_URL}/${slug}`);
   const event = await response.json();
 
-  return <main>EventPage</main>;
+  return (
+    <main>
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
+        <Image
+          src={event.imageUrl}
+          alt="Event background image"
+          className="object-cover blur-3xl z-0"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+
+        <div className="z-1 flex flex-col gap-6 lg:gap-16 lg:flex-row relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric"
+              })}
+            </p>
+
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 state-effects">
+              Get tickets
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }

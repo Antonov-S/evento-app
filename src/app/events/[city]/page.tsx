e73c45d1c9bd7b5fde9a51c6a95f9fc71db03522
Antonov-S <anto1nov1@gmail.com>
2024-03-04 01:17:48 +0200

    Fetch data in a component and wrap it in Suspense

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,8 +1,8 @@
-import { EventoEvent } from "@/app/lib/types";
-import { FETCH_URL } from "@/lib/constants";
-import H1 from "@/components/h1";
+import { Suspense } from "react";
+
 import EventsList from "@/components/events-list";
-import { sleep } from "@/lib/utils";
+import H1 from "@/components/h1";
+import Loading from "./loading";
 
 type EventsPageProps = {
   params: {
@@ -13,11 +13,6 @@ type EventsPageProps = {
 export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
 
-  await sleep(2000);
-
-  const response = await fetch(`${FETCH_URL}?city=${city}`);
-  const events: EventoEvent[] = await response.json();
-
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
@@ -26,7 +21,9 @@ export default async function EventsPage({ params }: EventsPageProps) {
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,11 +1,18 @@
 import { EventoEvent } from "@/app/lib/types";
 import EventCard from "./event-card";
+import { sleep } from "@/lib/utils";
+import { FETCH_URL } from "@/lib/constants";
 
 type EventsListProps = {
-  events: EventoEvent[];
+  city: string;
 };
 
-export default function EventsList({ events }: EventsListProps) {
+export default async function EventsList({ city }: EventsListProps) {
+  await sleep(2000);
+
+  const response = await fetch(`${FETCH_URL}?city=${city}`);
+  const events: EventoEvent[] = await response.json();
+
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map(event => (

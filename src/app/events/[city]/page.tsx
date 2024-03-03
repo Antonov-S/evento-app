ee567c779526e735c8c56a220b008ff59fdb1db6
Antonov-S <anto1nov1@gmail.com>
2024-03-03 19:48:59 +0200

    Creating loading components

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,8 +1,8 @@
 import { EventoEvent } from "@/app/lib/types";
 import { FETCH_URL } from "@/lib/constants";
-
 import H1 from "@/components/h1";
 import EventsList from "@/components/events-list";
+import { sleep } from "@/lib/utils";
 
 type EventsPageProps = {
   params: {
@@ -12,6 +12,8 @@ type EventsPageProps = {
 
 export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
+
+  await sleep(2000);
 
   const response = await fetch(`${FETCH_URL}?city=${city}`);
   const events: EventoEvent[] = await response.json();

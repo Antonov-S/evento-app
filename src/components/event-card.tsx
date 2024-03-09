b191773e474404fb7df93f8e7e8f1773d89af564
Antonov-S <anto1nov1@gmail.com>
2024-03-09 23:23:40 +0200

    Implement a scroll-based animation for the event cards with Framer Motion

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,17 +1,41 @@
+"use client";
+
+import { useRef } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import { EventoEvent } from "@/app/lib/types";
-import Link from "next/link";
 
 type EventCardProp = {
   event: EventoEvent;
 };
 
+const MotionLink = motion(Link);
+
 export default function EventCard({ event }: EventCardProp) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"]
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
   return (
-    <Link
+    <MotionLink
       href={`/event/${event.slug}`}
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
+      ref={ref}
+      style={{
+        // @ts-ignore
+        scale: scaleProgress,
+        // @ts-ignore
+        opacity: opacityProgress
+      }}
+      initial={{
+        opacity: 0,
+        scale: 0.8
+      }}
     >
       <section className="w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects">
         <Image
@@ -39,6 +63,6 @@ export default function EventCard({ event }: EventCardProp) {
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 }
